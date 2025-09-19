6090cad932be6ea239656e22e14e2e4506996e0f
2022184032-cmyk <2022184032@tukorea.ac.kr>
2025-09-19 23:22:16 +0900

    .

---
+++ 2DGP-Drill-03/character_moves.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+open_canvas()
+
+
+def moves_rectangle():
+    pass
+
+
+def moves_circle():
+    pass
+
+
+while True:
+    moves_rectangle()
+    moves_circle()
+    pass
+
+close_canvas()
