59ec37544ede75048965d6a61211a6945918f729
2022184032-cmyk <2022184032@tukorea.ac.kr>
2025-09-19 23:26:25 +0900

    캐릭터 출력 구현 완료

--- 2DGP-Drill-03/character_moves.py
+++ 2DGP-Drill-03/character_moves.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 open_canvas()
 
+boy = load_image('character.png')
 
 def moves_rectangle():
     print('moves_rectangle')
@@ -10,12 +11,15 @@ def moves_rectangle():
 
 def moves_circle():
     print('moves_circle')
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
 while True:
     moves_rectangle()
     moves_circle()
-    pass
+    break
 
 close_canvas()
